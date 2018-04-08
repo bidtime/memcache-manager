0dd9792358893a0eb60b58adc8c46d9d5b5da909
bidtime <bidtime@126.com>
2018-04-08 10:08:12 +0800

    modified memcache

--- src/main/java/org/bidtime/cache/Memcache.java
+++ src/main/java/org/bidtime/cache/Memcache.java
@@ -12,26 +12,40 @@ public class Memcache extends AbstractCache {
 	
 	private MemcachedClient memcacheClient;
 	
+  @Override
 	public void set(String key, int seconds, Object o) throws Exception {
 		memcacheClient.set(key, seconds, o);
 	}
 	
+  @Override
 	public void delete(String key) throws Exception {
 		memcacheClient.delete(key);
 	}
 	
+  @Override
 	public void replace(String key, int seconds, Object o) throws Exception {
 		memcacheClient.replace(key, seconds, o);
 	}
 	
-	public Object get(String key) throws Exception {
-		return memcacheClient.get(key);
+	@Override
+	public Object get(String key, boolean del) throws Exception {
+		Object o = memcacheClient.get(key);
+		if (del) {
+		  this.delete(key);
+		}
+		return o;
 	}
 	
-  public String getString(String key) throws Exception {
-    return String.valueOf(memcacheClient.get(key));
+  @Override
+  public String getString(String key, boolean del) throws Exception {
+    String val = String.valueOf(memcacheClient.get(key));
+    if (del) {
+      delete(key);
+    }
+    return val;
   }
   
+  @Override
   public void setString(String key, int seconds, String s) throws Exception {
     memcacheClient.set(key, seconds, s);
   }
